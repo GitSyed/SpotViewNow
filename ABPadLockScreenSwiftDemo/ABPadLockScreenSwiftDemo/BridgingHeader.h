9c88b8ce83a093b1ffb9e934005aeee3af0fa090
GitSyed <syedmm19@gmail.com>
2017-08-11 12:06:17 -0700

    removed Pad Lock

--- ABPadLockScreenSwiftDemo/ABPadLockScreenSwiftDemo/BridgingHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  BridgingHeader.h
-//  ABPadLockScreenSwiftDemo
-//
-//  Created by Aron Bury on 13/09/2015.
-//  Copyright © 2015 Aron Bury. All rights reserved.
-//
-
-#import "ABPadLockScreen.h"
